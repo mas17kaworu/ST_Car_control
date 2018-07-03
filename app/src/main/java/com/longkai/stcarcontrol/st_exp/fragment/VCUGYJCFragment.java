0697b845edf9c362a4b9a208049f3bba8608c4c1
Long Kai   Karl <longkai_1990@aliyun.com>
2018-07-03 23:05:34 +0800

    lll

--- app/src/main/java/com/longkai/stcarcontrol/st_exp/fragment/VCUGYJCFragment.java
+++ app/src/main/java/com/longkai/stcarcontrol/st_exp/fragment/VCUGYJCFragment.java
@@ -17,13 +17,20 @@ import com.longkai.stcarcontrol.st_exp.R;
 public class VCUGYJCFragment extends Fragment implements View.OnClickListener {
     private View mView;
 
+    private ImageView iv_jdq1, iv_jdq2, iv_jdq3;
+
+
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container,
                              Bundle savedInstanceState) {
         mView = inflater.inflate(R.layout.fragment_vcu_gyjc, container, false);
+        iv_jdq1 = (ImageView) mView.findViewById(R.id.iv_vcu_gyjc_jdq1);
+        iv_jdq2 = (ImageView) mView.findViewById(R.id.iv_vcu_gyjc_jdq2);
+        iv_jdq3 = (ImageView) mView.findViewById(R.id.iv_vcu_gyjc_jdq3);
         return mView;
     }
+
 
     @Override
     public void onClick(View v) {
