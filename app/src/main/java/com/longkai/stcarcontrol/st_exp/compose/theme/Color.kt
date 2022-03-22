c29272a7a29f2d374e70dda6dc31af4a2ead49f2
Chaofan Zhang <zhang.chf@qq.com>
2022-03-22 10:54:20 +0800

    Setup compose

---
+++ app/src/main/java/com/longkai/stcarcontrol/st_exp/compose/theme/Color.kt
@@ -0,0 +1,8 @@
+package com.example.composestart.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+val ColorPrimaryLight = Color(0xFF3abee5)
+val ColorPrimary = Color(0xFF3F51B5)
+val ColorPrimaryDark = Color(0xFF303F9F)
+val ColorAccent = Color(0xFF32f0a2)
