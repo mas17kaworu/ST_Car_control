c0a79dbf4935593746b11cc531dd0ced052ae792
Kai Long <kai.long@goat.com>
2023-02-16 13:22:36 +0800

    upgrade version number

--- app/src/androidTest/java/com/longkai/stcarcontrol/st_exp/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/com/longkai/stcarcontrol/st_exp/ExampleInstrumentedTest.java
@@ -19,8 +19,5 @@ public class ExampleInstrumentedTest {
     @Test
     public void useAppContext() throws Exception {
         // Context of the app under test.
-        Context appContext = InstrumentationRegistry.getTargetContext();
-
-        assertEquals("com.longkai.stcarcontrol.st_exp", appContext.getPackageName());
     }
 }
